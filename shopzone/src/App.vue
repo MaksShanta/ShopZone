82b1cae22c266ef99d05de7d2e7ae3ddc0769888
Shanta Max <shanta.maksym@student.uznhu.edu.ua>
2026-05-29 10:35:24 +0200

    Start project

---
+++ shopzone/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold text-green-600">
+    ShopZone працює!
+  </h1>
+</template>
