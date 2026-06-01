774b7383fd3f3ba4a93a944dbfd1748a45c13856
Shanta Max <shanta.maksym@student.uznhu.edu.ua>
2026-06-01 13:04:54 +0200

    fix bug

--- shopzone/src/components/Sidebar.vue
+++ shopzone/src/components/Sidebar.vue
@@ -6,7 +6,7 @@
       <li
         v-for="category in productStore.categories"
         :key="category"
-        @click="productStore.setCategory(category)"
+        @click="selectCategory(category)"
         class="cursor-pointer rounded px-2 py-1 hover:bg-green-50 hover:text-green-600"
         :class="productStore.selectedCategory === category ? 'bg-green-100 text-green-700' : ''"
       >
@@ -17,7 +17,14 @@
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router'
 import { useProductStore } from '../stores/productStore'
 
+const router = useRouter()
 const productStore = useProductStore()
+
+function selectCategory(category) {
+  productStore.setCategory(category)
+  router.push('/catalog')
+}
 </script>
